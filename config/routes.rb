5cb6ca5d66097443e1f9b5231a4ae14a4a6e2de0
AWITI0301 <johnoketchawiti@gmail.com>
2022-10-09 20:51:27 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  # resources :hero_powers
+  # resources :powers
+  # resources :heros
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :heroes ,only:[:index,:show,]
+  resources :heros ,only:[:index,:show,]
   resources :powers ,only:[:index,:show,:update,:create]
   get '/hero_powers', to: 'hero_powers#index'
   post '/hero_powers', to: 'hero_powers#create'
